2d202880b0d3ad000203689e099b57072cec6497
Giacomo Russo <giacomo.russo@stentle.com>
2018-01-23 15:22:39 +0100

    Add brightness scripts for Macbook Pro 11,5

---
+++ bin/lower-brightness.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo 215 | sudo tee /sys/class/backlight/gmux_backlight/brightness
---
+++ bin/raise-brightness.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo 1000 | sudo tee /sys/class/backlight/gmux_backlight/brightness
