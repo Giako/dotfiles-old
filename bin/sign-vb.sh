492241acaa912b8971e41cbdb1b56732315b679d
Giacomo Russo <giako@giako.space>
2018-03-17 19:27:56 +0100

    Point correctly MOK and DEK files

--- bin/sign-vb.sh
+++ bin/sign-vb.sh
@@ -3,4 +3,4 @@
 # Author: Giako
 echo "Signin script /usr/src/linux-headers-$(uname -r)/scripts/sign-file";
 echo "Module name: $(modinfo -n vboxdrv)";
-sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha-256 ./MOK.priv ./MOK.der $(modinfo -n vboxdrv)
+sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha-256 /home/giako/MOK.priv /home/giako/MOK.der $(modinfo -n vboxdrv)
